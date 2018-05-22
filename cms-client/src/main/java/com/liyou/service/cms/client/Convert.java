95c02414abc9a28f5cbaa69895a3da237af44403
linxiaohui <250610578@qq.com>
2018-05-22 10:34:30 +0800

    增加配置

---
+++ cms-client/src/main/java/com/liyou/service/cms/client/Convert.java
@@ -0,0 +1,6 @@
+package com.liyou.service.cms.client;
+
+public interface Convert<T> {
+
+    void convert(T t,ResourceItem resourceItem);
+}
--- cms-client/src/main/java/com/liyou/service/cms/client/ResourceItem.java
+++ cms-client/src/main/java/com/liyou/service/cms/client/ResourceItem.java
@@ -1,6 +1,10 @@
 package com.liyou.service.cms.client;
 
+import com.liyou.framework.common.utils.JSONUtils;
+
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.function.Function;
 
 /***
  *
@@ -8,6 +12,8 @@ import java.io.Serializable;
  *   @date 2018/5/21
  ***/
 public class ResourceItem implements Serializable{
+
+
 
     private Long id;
     private String json;
@@ -44,5 +50,28 @@ public class ResourceItem implements Serializable{
 
     public void setSortNumber(Integer sortNumber) {
         this.sortNumber = sortNumber;
+    }
+
+    public <T> T convert(Class<T> cla){
+
+        T obj = null;
+        try {
+            obj = JSONUtils.toObject(this.json,cla);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+
+    public <T> T convert(Class<T> cla,Convert<T> convert){
+
+        T obj = null;
+        try {
+            obj = JSONUtils.toObject(this.json,cla);
+            convert.convert(obj,this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return obj;
     }
 }
